eba77fa3de76d28926465763b7ed3bb4567b19a4
Heiner Peuser <heiner.peuser@weweave.net>
2017-04-06 22:11:45 +0200

    Added customer creation

--- src/dao/CustomerDao.ts
+++ src/dao/CustomerDao.ts
@@ -8,4 +8,8 @@ export class CustomerDao extends Dao<Customer> {
     protected getRepository(): Repository<Customer> {
         return this.getEm().getRepository(Customer);
     }
+
+    public async getByEmail(email: string): Promise<Customer> {
+        return this.getRepository().findOne({email: email});
+    }
 }
--- src/util/OrderNotificationMapper.ts
+++ src/util/OrderNotificationMapper.ts
@@ -1,30 +1,47 @@
 import * as jsonMapper from 'json-mapper-json';
 import * as xmlJs from 'xml-js';
+import { Container } from "typedi";
 import { Broker } from '../entity/Broker';
 import { Customer } from '../entity/Customer';
 import { Order } from '../entity/Order';
 import { OrderItem } from '../entity/OrderItem';
+import { CustomerDao } from "../dao/CustomerDao";
 
 export class OrderNotificationMapper {
     public static map(input: string|Object, template: Object): Promise<Order> {
         return new Promise<Order>((resolve, reject) => {
             let convertedInput: Object = OrderNotificationMapper.getInputAsJson(input);
-            console.log("------> IN: " + JSON.stringify(convertedInput));
+            //console.log("------> IN: " + JSON.stringify(convertedInput));
             jsonMapper(convertedInput, template).then((result) => {
-                console.log("---------> OUT: " + JSON.stringify(result));
+                //console.log("---------> OUT: " + JSON.stringify(result));
                 let order: Order = new Order();
                 order.referenceId = result.id;
-                order.customer = new Customer();
-
-                // TODO
-                resolve(order);
+                OrderNotificationMapper.getOrCreateCustomer(result.customer).then((customer) => {
+                    order.customer = customer;
+                    resolve(order);
+                });
             }).catch((e) => reject(e));
+        });
+    }
+
+    private static async getOrCreateCustomer(json: any): Promise<Customer> {
+        let customerDao: CustomerDao = Container.get(CustomerDao);
+        let customer: Customer = await customerDao.getByEmail(json.email);
+        return new Promise<Customer>((resolve, reject) => {
+            if (customer == undefined || customer == null) {
+                customer = new Customer();
+                customer.firstname = json.firstname;
+                customer.lastname = json.lastname;
+                customer.company = json.company;
+                customer.email = json.email;
+                customer.country = json.country;
+            }
+            resolve(customer);
         });
     }
 
     private static getInputAsJson(input: string|Object): Object {
         if (typeof input === "string") {
-            console.log("STRING!!!!!!!!!!!!!!!!!!!!");
             if (input.trim().startsWith("<")) {
                 return xmlJs.xml2js(input, {compact: true});
             } else {
