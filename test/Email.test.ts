d0fdaf0e3fb42c34b68cae3af4f912f2fb031219
Heiner <heiner.peuser@gmail.com>
2017-11-04 18:29:02 +0100

    Added test cases for Email.renderParamString()

--- test/Email.test.ts
+++ test/Email.test.ts
@@ -33,5 +33,34 @@ describe("Email", () => {
             let result = Email.renderParamString(s, params);
             expect(result).to.equal("Hello John Doe!");
         });
+
+        it("should work well for a real-world subject", () => {
+            let params = {
+                firstname: "John",
+                lastname: "Doe",
+                siteUrl: "https://weweave.net",
+                daysRemaining: 30,
+                product: "DNN Google Analytics Advanced"
+            };
+            let s = "Your License Key for {{product}} will expire in {{daysRemaining}} days";
+            let result = Email.renderParamString(s, params);
+            expect(result).to.equal("Your License Key for DNN Google Analytics Advanced will expire in 30 days");
+        });
+
+        it("should handle null params", () => {
+            let payload: any = {
+                daysRemainings: 5
+            };
+            let params = {
+                firstname: "John",
+                lastname: "Doe",
+                siteUrl: "https://weweave.net",
+                daysRemaining: payload.daysRemaining,
+                product: "DNN Google Analytics Advanced"
+            };
+            let s = "Your License Key for {{product}} will expire in {{daysRemaining}} days";
+            let result = Email.renderParamString(s, params);
+            expect(result).to.equal("Your License Key for DNN Google Analytics Advanced will expire in undefined days");
+        });
     });
 });
